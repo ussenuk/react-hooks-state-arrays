624afb789322d05b381bcb19242cf07f93ebd7e9
ussenuk <ukimanuka@gmail.com>
2023-12-03 18:02:52 +0300

    understanding state-arrays

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -4,21 +4,77 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
-  function handleAddFood() {
-    const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+  const [filterBy, setFilterBy] = useState("All");
+
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+
+  function handleAddFood() {
+    const newFood = getNewRandomSpicyFood();
+    // console.log(newFood);
+
+    // if you want to add to an array use spread operator
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+  }
+
+  function handleLiClick(id){
+
+    // if you want to update use map
+
+      const newFoodArray = foods.map((food) => {
+        if (food.id === id) {
+          return {
+            ...food,
+            heatLevel: food.heatLevel + 1,
+          };
+        } else {
+          return food;
+        }
+      });
+      setFoods(newFoodArray);
+
+      // if you want to delete use filter
+
+    // const newFoodArray = foods.filter((food)=> food.id !== id);
+    // setFoods(newFoodArray);
+
+  }
+
+
+  // const foodList = foods.map((food) => (
+  //   <li key={food.id} onClick={()=> handleLiClick(food.id)}>
+  //     {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  //   </li>
+  // ));
+
   return (
     <div>
-      <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
+    <select name="filter" onChange={handleFilterChange}>
+      <option value="All">All</option>
+      <option value="American">American</option>
+      <option value="Sichuan">Sichuan</option>
+      <option value="Thai">Thai</option>
+      <option value="Mexican">Mexican</option>
+    </select>
+    <button onClick={handleAddFood}>Add New Food</button>
+    <ul>{foodList}</ul>
     </div>
   );
 }
